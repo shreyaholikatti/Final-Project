99bc8a178d75a5d72c58a3384f0aca8da5f78c14
Cailey Murad <52009084+caileymurad@users.noreply.github.com>
2019-07-26 00:13:04 -0700

    Update cons.js

--- cons.js
+++ cons.js
@@ -1,6 +1,13 @@
+// function handle(e){
+//     if(e.keyCode === 13){
+//         e.preventDefault(); // Ensure it is only this code that rusn
+ 
+//     }
+// }
+
 function handle(e){
     if(e.keyCode === 13){
-        e.preventDefault(); // Ensure it is only this code that rusn
+       parent.location='https://shreyaholikatti.github.io/Final-Project/number_of_people.html'
  
     }
 }
