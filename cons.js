15016f7e4419e95db4f1fa28ff34cb47380408e0
Cailey Murad <52009084+caileymurad@users.noreply.github.com>
2019-07-26 00:25:34 -0700

    Update cons.js

--- cons.js
+++ cons.js
@@ -1,12 +1,6 @@
-// function handle(e){
-//     if(e.keyCode === 13){
-//         e.preventDefault(); // Ensure it is only this code that rusn
- 
-//     }
-// }
-
 function handle(e){
     if(e.keyCode === 13){
-        response.sendRedirect("https://shreyaholikatti.github.io/Final-Project/num_of_people.html");
+        e.preventDefault(); // Ensure it is only this code that rusn
+ 
     }
 }
