1bc075b049ab75c4af65688c5ac1278fc5934d5f
Cailey Murad <52009084+caileymurad@users.noreply.github.com>
2019-07-26 10:10:20 -0700

    Update cons.js

--- cons.js
+++ cons.js
@@ -1,5 +1,5 @@
 function handle(e){
-    if(e.keyCode === 13);{
+    if(e.keyCode === 13){
         e.preventDefault();
         window.location.href = "https://shreyaholikatti.github.io/Final-Project/num_of_people.html"
     }
