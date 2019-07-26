a6c4a6fe3995e17f6b26ac9c1a2e9cfc4b0c1947
Cailey Murad <52009084+caileymurad@users.noreply.github.com>
2019-07-26 08:52:49 -0700

    Update cons.js

--- cons.js
+++ cons.js
@@ -1,6 +1,13 @@
-function handle(e){
-    if(e.keyCode === 13){
-        e.preventDefault(); // Ensure it is only this code that rusn
+// function handle(e){
+//     if(e.keyCode === 13){
+//         e.preventDefault(); // Ensure it is only this code that rusn
  
-    }
+//     }
+// }
+
+function myFunction(event) {
+  var x = event.keyCode;
+  if (x == 27) {  // 27 is the ESC key
+    alert ("You pressed the Escape key!");
+  }
 }
