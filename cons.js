1f08d20a728a07ed0646bb470ad0bf64eab54f07
Cailey Murad <52009084+caileymurad@users.noreply.github.com>
2019-07-26 09:09:32 -0700

    Update cons.js

--- cons.js
+++ cons.js
@@ -1,8 +1,7 @@
 function handle(e){
     if(e.keyCode === 13){
         e.preventDefault(); // Ensure it is only this code that rusn
-
+    }
      //      alert("Enter was pressed was presses");
         window.location.href = "num_of_people.html"
-    }
 }
